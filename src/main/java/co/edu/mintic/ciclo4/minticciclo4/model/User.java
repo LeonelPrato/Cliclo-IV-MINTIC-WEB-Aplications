3282f2cb3b7599985544f819a47a483d760f4b8c
LeonelPrato <leonel.prato.mt@correo.usa.edu.co>
2021-11-17 22:40:48 -0500

    Primer Envio Prato remove Import unusually

--- src/main/java/co/edu/mintic/ciclo4/minticciclo4/model/User.java
+++ src/main/java/co/edu/mintic/ciclo4/minticciclo4/model/User.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
 
