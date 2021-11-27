b398801a010353109f5ab2d230348826a7564021
unknown <leonel.prato.mt@correo.usa.edu.co>
2021-11-27 17:12:22 -0500

    Pare Presentar Reto 1

--- src/main/java/co/edu/mintic/ciclo4/minticciclo4/MinticCiclo4Application.java
+++ src/main/java/co/edu/mintic/ciclo4/minticciclo4/MinticCiclo4Application.java
@@ -2,7 +2,6 @@ package co.edu.mintic.ciclo4.minticciclo4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class MinticCiclo4Application {
 
